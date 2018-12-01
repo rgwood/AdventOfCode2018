c0fdcc341b36e4fb41dc6a52dc565bfa15ef3499
Reilly <reilly.wood@icloud.com>
2018-12-01 07:48:34 -0800

    Initial commit

---
+++ 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalFrequency = 0;
+            var totalFrequenciesSeen = new HashSet<int>();
+            var listOfFrequencies = new List<int>();
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    String line = sr.ReadLine();
+                    var parsedFrequency = Int32.Parse(line);
+                    listOfFrequencies.Add(parsedFrequency);
+                }
+            }
+
+            while(true)
+            {
+                foreach (var freq in listOfFrequencies)
+                {
+                    totalFrequenciesSeen.Add(totalFrequency);
+                    totalFrequency += freq;
+                    if(totalFrequenciesSeen.Contains(totalFrequency))
+                    {
+                        Console.WriteLine($"First frequency seen twice: {totalFrequency}");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
